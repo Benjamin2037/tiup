a627f6d52df1149892047582321ed2a69c40244d
Benjamin2037 <bear.c@pingcap.com>
2025-11-20 13:23:07 +0800

    Fix complile problem

--- pkg/logger/printer/logger.go
+++ pkg/logger/printer/logger.go
@@ -2,9 +2,6 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-	// Info logs an info level message.
-	func (l *Logger) Info(args ...interface{}) {
-		panic("unimplemented")
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
